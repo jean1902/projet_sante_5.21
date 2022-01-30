685a2c5884c9d236d921b295523715d8b526df2b
jean1902 <cin71781@gmail.com>
2022-01-30 01:15:16 +0100

    01302022

--- script.js
+++ script.js
@@ -44,3 +44,42 @@ fetch(`https://api.covid19api.com/summary`)
 //     console.log(data)
     
 // })
+ 
+//navbar_menu
+
+let btn_select=document.querySelector('.btn_menu_head');
+let div_menu =document.querySelector('.div_menu');
+let result_search_api =document.querySelector('.result_search_api')
+let input_header = document.querySelector('.input_header')
+let form_header =document.querySelector('.form_header')
+console.log(form_header)
+btn_select.addEventListener('click' ,()=>{
+  console.log('hello')
+  div_menu.classList.toggle('active')
+  console.log(div_menu) 
+})
+
+//search
+form_header.addEventListener('submit',()=>{
+  console.log(input_header.value)
+  fetch(`https://api.covid19api.com/summary?query=${input_header.value}`)
+  .then(res => res.json())
+  .then( data => console.log(data))
+})
+
+
+
+
+//slider multiple
+$('.bg_header').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 4000,
+});
+        
+let prev= document.querySelector('.slick-prev').innerHTML =` <img src="image/left-arrow_left.png" alt="left_arrows" class="btn-size">`
+console.log(prev);
+let next= document.querySelector('.slick-next').innerHTML=` <img src="image/left-arrow_right.png" alt="right_arrows" class="btn-size" >`
+
+     
